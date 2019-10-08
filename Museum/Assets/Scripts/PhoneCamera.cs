87436098d5d7aae10b507774dd3af7fffa888d66
Hugo <bombix34@gmail.com>
2019-10-08 19:23:36 +0200

    [ADD] début script camera

---
+++ Museum/Assets/Scripts/PhoneCamera.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneCamera : MonoBehaviour
+{
+
+    private bool camAvailable;
+    private WebCamTexture backCamera;
+    private Texture defaultBackground;      //go back to previous background is something go wrong
+
+    [SerializeField] RawImage background;
+    [SerializeField] AspectRatioFitter fit;
+
+//BASE FUNCTION_____________________________________________________________________________________________________________
+
+    private void Start()
+    {
+        if(InitCamera())
+        {
+            DisplayCamera();
+        }
+
+    }
+
+    private void Update()
+    {
+        UpdateCameraDisplay();
+    }
+
+//CAMERA PART________________________________________________________________________________________________________________
+
+    private bool InitCamera()
+    {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log("No camera detected");
+            camAvailable = false;
+            return false;
+        }
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+            if (!devices[i].isFrontFacing)
+            {
+                backCamera = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+            }
+        }
+        if (backCamera == null)
+        {
+            Debug.Log("Unable to find back camera");
+            return false;
+        }
+        return true;
+    }
+
+    private void DisplayCamera()
+    {
+        backCamera.Play();
+        background.texture = backCamera;
+
+        camAvailable = true;
+    }
+
+    private void UpdateCameraDisplay()
+    {
+        if (!camAvailable)
+            return;
+        float ratio = (float)backCamera.width / (float)backCamera.height;
+        fit.aspectRatio = ratio;
+
+        float scaleY = backCamera.videoVerticallyMirrored ? -1f : 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -backCamera.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0f, 0f, orient);
+    }
+}
